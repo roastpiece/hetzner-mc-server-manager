69e39f9b20133c0b04fbc3bc1e636dc6473b5476
Michael Noser <michael@noser.moe>
2025-11-10 16:06:47 +0100

    refactor: enhance GET request to fetch and return server status as JSON

--- src/routes/api/minecraft/status/+server.ts
+++ src/routes/api/minecraft/status/+server.ts
@@ -2,5 +2,14 @@ import { MC_SERVER_IP } from "$env/static/private";
 import type { RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async () => {
-    return fetch(`https://api.mcsrvstat.us/3/${MC_SERVER_IP}`);
+    const status = await fetch(`https://api.mcsrvstat.us/3/${MC_SERVER_IP}`, {
+        headers: {
+            "User-Agent": "Hetzner-MC-Server-Manager",
+        },
+    });
+    const data = await status.json();
+
+    return new Response(JSON.stringify(data), {
+        headers: { "Content-Type": "application/json" },
+    });
 };
